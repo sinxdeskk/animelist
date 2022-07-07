eb1d8096d2031d4add55e44c1982713a42fce5b7
sinxdeskk <richard.santos21@hotmail.com>
2022-07-06 23:56:33 -0300

    adicionando h1

--- inicio.php
+++ inicio.php
@@ -15,6 +15,7 @@ if(!isset($_SESSION)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <h1>BEM VINDO PORRA</h1>
 </head>
 <body>
     
